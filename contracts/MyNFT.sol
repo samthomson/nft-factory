1c4bb111a68dc36cafd71f62952e32da45c86e87
Sam <samt@samt.st>
2022-07-03 13:18:37 +0300

    first commit

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,32 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// ecr721 (original NFT) standard
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// will use to increment the ID assigned to minted NFTs
+import "@openzeppelin/contracts/utils/Counters.sol";
+// so only this contract's owner can use it to mint NFTs
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+	// pass in this contracts name and it's symbol
+    constructor() ERC721("MyNFT", "NFTravel") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# nft minting
+
+based on https://ethereum.org/en/developers/tutorials/how-to-write-and-deploy-an-nft
+
+- install deps in package.json
+- set api url and wallet private key into .env
+- change constructor args in `contracts/MyNFT.sol` to make different NFT name
+- set network name (in two places) in config: `hardhat.config.js`
+- compile `npx hardhat compile`
+- deploy contract `npx hardhat --network goerli run scripts/deploy.js``
+	- take the outputted contract address and store in `MINTING_CONTRACT_ADDRESS`
+
+- [upload a file to IPFS](https://app.pinata.cloud/)
+- copy/rename `nft-seed-data/nft-metadata-sample.json` and fill in with your uploaded file's IPFS id, and other metadata.
+- upload that metadata file to IPFS, then store it's IPFS id as `NFT_METADATA_IPFS_ID` in `env`
+- run mint contract `node scripts/mint-nft.js`
