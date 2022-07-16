df9e3745c7513c56a4cad8fb92c9ee46ea6be312
Sam <samt@samt.st>
2022-07-16 20:28:06 +0300

    nest existing scripts

---
+++ scripts/webstorage.ts
@@ -0,0 +1,15 @@
+
+
+/**
+ * 
+ * path structure
+ * 
+ * root: /dapps/nft-factory/
+ * images: ./images
+ * nfts: ./nfts/${collection}/
+ * 
+ * - query and retrieve all nfts metadatas files
+ * - store an image in ipfs
+ * - store an nft metadata file
+ * 
+ */
