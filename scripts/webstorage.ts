b22bbb52979e87c60f6d4307903ad226c783c3f0
Sam <samt@samt.st>
2022-07-17 09:22:25 +0300

    basic web3 storage writing

--- scripts/webstorage.ts
+++ scripts/webstorage.ts
@@ -1,3 +1,6 @@
+import { Web3Storage, File, getFilesFromPath } from 'web3.storage'
+require("dotenv").config()
+// require('dotenv').config({path: __dirname + '../.env'})
 
 
 /**
@@ -13,3 +16,35 @@
  * - store an nft metadata file
  * 
  */
+const makeStorageClient = () => {
+	const token = (process.env.WEB3STORAGE_API_KEY as string)
+	// @ts-ignore
+	return new Web3Storage({ token })
+}
+
+const getFiles = async (path: string) => {
+	const files = await getFilesFromPath(path)
+	console.log(`read ${files.length} file(s) from ${path}`)
+	return files
+}
+
+const storeFiles = async (files: File[]) => {
+	const client = makeStorageClient()
+	const cid = await client.put(files)
+	console.log('stored files with cid:', cid)
+	return cid
+}
+
+const testStoreSomeLocalFiles = async () => {
+	const path = './test-files/test'
+	const files = await getFiles(path)
+
+	console.log('files', files)
+
+	const client = makeStorageClient()
+	const cid = await client.put(files)
+	console.log('stored files with cid:', cid)
+	return cid
+}
+
+testStoreSomeLocalFiles()
